dba6828479fcd8af746701e4a58c0a673a540169
Chad <chadly629@gmail.com>
2024-08-29 22:53:49 -0700

    update allowed image sizes

--- civitai/schemas/__init__.py
+++ civitai/schemas/__init__.py
@@ -28,10 +28,17 @@ class FromTextSchema(BaseModel):
 
     @validator('params')
     def params_validator(cls, v):
-        if 'width' in v and not (1 <= v['width'] <= 1024):
-            raise ValueError("Width must be between 1 and 1024")
-        if 'height' in v and not (1 <= v['height'] <= 1024):
-            raise ValueError("Height must be between 1 and 1024")
+        # CEB 8/28/2024 - the web UI allows larger than 1024; standard sizes are
+        # square: 1024 x 1024
+        # landscape: 1216 x 832
+        # portrait: 832 x 1216
+        sizes = ((1024,1024), (1216,832), (832,1216))
+        #if 'width' in v and not (1 <= v['width'] <= 1024):
+        #    raise ValueError("Width must be between 1 and 1024")
+        #if 'height' in v and not (1 <= v['height'] <= 1024):
+        #    raise ValueError("Height must be between 1 and 1024")
+        if (v['width'],v['height']) not in sizes:
+            raise ValueError(f"width,height must be one of {str(sizes)}")
         return v
 
     class Config:
